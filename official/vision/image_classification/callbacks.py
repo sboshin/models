a55be69253b8da1da6c661ca4f034e94fcda1056
Samuel Oshin <sboshin@amazon.com>
2020-09-09 18:12:20 +0000

    Removing unneeded callbacks

--- official/vision/image_classification/callbacks.py
+++ official/vision/image_classification/callbacks.py
@@ -42,34 +42,34 @@ def get_callbacks(model_checkpoint: bool = True,
   """Get all callbacks."""
   model_dir = model_dir or ''
   callbacks = []
-  if model_checkpoint:
-    ckpt_full_path = os.path.join(model_dir, 'model.ckpt-{epoch:04d}')
-    callbacks.append(tf.keras.callbacks.ModelCheckpoint(
-        ckpt_full_path, save_weights_only=True, verbose=1))
-  if include_tensorboard:
-    callbacks.append(
-        CustomTensorBoard(
-            log_dir=model_dir,
-            track_lr=track_lr,
-            initial_step=initial_step,
-            write_images=write_model_weights))
+  #if model_checkpoint:
+  #  ckpt_full_path = os.path.join(model_dir, 'model.ckpt-{epoch:04d}')
+  #  callbacks.append(tf.keras.callbacks.ModelCheckpoint(
+  #      ckpt_full_path, save_weights_only=True, verbose=1))
+  #if include_tensorboard:
+  #  callbacks.append(
+  #      CustomTensorBoard(
+  #          log_dir=model_dir,
+  #          track_lr=track_lr,
+  #          initial_step=initial_step,
+  #          write_images=write_model_weights))
   if time_history:
     callbacks.append(
         keras_utils.TimeHistory(
             batch_size,
             log_steps,
             logdir=model_dir if include_tensorboard else None))
-  if apply_moving_average:
+  #if apply_moving_average:
     # Save moving average model to a different file so that
     # we can resume training from a checkpoint
-    ckpt_full_path = os.path.join(
-        model_dir, 'average', 'model.ckpt-{epoch:04d}')
-    callbacks.append(AverageModelCheckpoint(
-        update_weights=False,
-        filepath=ckpt_full_path,
-        save_weights_only=True,
-        verbose=1))
-    callbacks.append(MovingAverageCallback())
+  #  ckpt_full_path = os.path.join(
+  #      model_dir, 'average', 'model.ckpt-{epoch:04d}')
+  #  callbacks.append(AverageModelCheckpoint(
+  #      update_weights=False,
+  #      filepath=ckpt_full_path,
+  #      save_weights_only=True,
+  #      verbose=1))
+  #  callbacks.append(MovingAverageCallback())
   if(backup_dir is None):
       raise ValueError("Backup dir not specified")
   callbacks.append(CustomBackupAndRestore(backup_dir))
